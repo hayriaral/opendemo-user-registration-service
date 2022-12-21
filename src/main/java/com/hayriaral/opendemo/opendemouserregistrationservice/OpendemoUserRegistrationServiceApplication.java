27a2437abfbb262b07eaf1e16487397f1407d345
Hayri Aral <hayriaral@gmail.com>
2022-12-21 19:51:33 +0300

    Spring Boot initialized

---
+++ src/main/java/com/hayriaral/opendemo/opendemouserregistrationservice/OpendemoUserRegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hayriaral.opendemo.opendemouserregistrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpendemoUserRegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpendemoUserRegistrationServiceApplication.class, args);
+	}
+
+}
